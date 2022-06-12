34ed8c4a4451922d54fd314d35b74c805622e87d
Konoshevskyi Denys <107351163+kRabUz@users.noreply.github.com>
2022-06-12 17:09:22 +0300

    Add files via upload

---
+++ Lab4/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4
+{
+    abstract class Employee
+    {
+        public abstract int Experience { get; set; }
+        public abstract int Salary { get; set; }
+        public abstract int Holiday { get; set; }
+        public Employee(int experience, int salary, int holiday)
+        {
+            Experience = experience;
+            Salary = salary;
+            Holiday = holiday;
+        }
+        public abstract int GetExperience();
+        public abstract int GetSalary();
+        public abstract int GetHoliday();
+    }
+}
